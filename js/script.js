901481e9a39b470fa910ecf5952ed7ee08d97bfd
Dominik-Saur <159741529+Dominik-Saur@users.noreply.github.com>
2024-10-02 22:55:55 +1300

    update submit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 var url = atob('aHR0cHM6Ly9hcGkuc3RhdGljZm9ybXMueHl6')
-var query = document.querySelector('form').getAttribute('action');
-document.querySelector('form').addEventListener('submit', function (event) {
+var form = document.querySelector('form');
+var query = form.getAttribute('action');
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     fetch(`${url}/${query}`, {
         method: 'POST',
         body: new URLSearchParams(new FormData(event.target))
-    }).then(this.reset.bind(this));
+    }).then(
+		{
+			this.reset.bind(this);
+			form.reset();
+		}
+	);
 });
